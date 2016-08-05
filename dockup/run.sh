207e9c85eb7a0133e788eaf1390af1d35f59eb04
Rasheed Amir <rasheed@aurorasolutions.io>
2016-08-05 13:18:33 +0200

    backup restore volume using docker

---
+++ dockup/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ "$RESTORE" == "true" ]]; then
+  ./restore.sh
+else
+  if [ -n "$CRON_TIME" ]; then
+    env | grep -v 'affinity:container' | sed -e 's/^\([^=]*\)=\(.*\)/export \1="\2"/' > /env.conf # Save current environment
+    echo "${CRON_TIME} . /env.conf && /backup.sh 2>&1 | logger -t dockup-cron-${BACKUP_NAME}" > /crontab.conf
+    crontab  /crontab.conf
+    echo "=> Running dockup backups as a cronjob for ${CRON_TIME}"
+    exec cron -f
+  else
+    ./backup.sh
+  fi
+fi
