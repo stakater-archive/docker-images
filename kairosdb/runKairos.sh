ef0edd111a5434f0ac624274e8816c21f89a7e76
Rasheed Amir <rasheed@aurorasolutions.io>
2016-10-14 22:07:56 +0200

    set kairosdb HEALTH_RESPONSE_CODE through environment variable

--- kairosdb/runKairos.sh
+++ kairosdb/runKairos.sh
@@ -4,6 +4,7 @@ export PORT_TELNET=${PORT_TELNET:-4242}
 export PORT_HTTP=${PORT_HTTP:-8080}
 export CASS_HOSTS=${CASS_HOSTS:-"localhost:9160"}
 export REPFACTOR=${REPFACTOR:-1}
+export HEALTH_RESPONSE_CODE=${HEALTH_RESPONSE_CODE:-200}
 
 function main {
   echo "- env ------------------------------------------------------------------"
